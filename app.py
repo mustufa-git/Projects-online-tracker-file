25727a69a4fa3e46ed76c55bd283f8bf1ab9d1ac
mustufa-git <golammustufa847@gmail.com>
2026-04-02 11:55:36 +0600

    Update app.py

--- app.py
+++ app.py
@@ -342,4 +342,9 @@ def get_activity():
 if __name__ == "__main__":
     init_db()
     port = int(os.environ.get("PORT", 5000))
+    with app.app_context():
+    init_db()
+
+if __name__ == '__main__':
+    app.run(debug=True)
     app.run(host="0.0.0.0", port=port, debug=False)
